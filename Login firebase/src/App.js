2a7f06fb1ca838e8948a1540b018bbc70e0a1f97
sairamm-s <58135922+sairamm-s@users.noreply.github.com>
2020-08-20 11:20:10 +0530

    Add files via upload

---
+++ Login firebase/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './Components/Login';
+class App extends Component {
+  render() {
+    return (
+      <div className='App container'>
+        <Login />
+      </div>
+    );
+  }
+}
+
+export default App;
